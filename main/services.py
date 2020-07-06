6543b1d2b28cecc0febf7a7985e6a0e05ee1ba0d
DebDoDab <debbius3@gmail.com>
2020-07-06 21:30:31 +0300

    Fix Changed database location to the project dir Renamed LETTERS to POSSIBLE_CHARS

--- main/services.py
+++ main/services.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import Session
 import sys
 from . import models, schemas
 
-LETTERS = string.ascii_letters + string.digits
+POSSIBLE_CHARS = string.ascii_letters + string.digits
 
 
 def get_original_url(db: Session, short_link: str) -> Union[str, None]:
@@ -19,9 +19,9 @@ def create_short_link(db: Session, url: str) -> str:
     if db_link := db.query(models.Link).filter(models.Link.url == url).first():
         return db_link.link
 
-    short_link = "".join(random.choices(LETTERS, k=5))
+    short_link = "".join(random.choices(POSSIBLE_CHARS, k=5))
     while get_original_url(db, short_link):
-        short_link = "".join(random.choices(LETTERS, k=5))
+        short_link = "".join(random.choices(POSSIBLE_CHARS, k=5))
 
     db_link = models.Link(link=short_link, url=url)
     db.add(db_link)
