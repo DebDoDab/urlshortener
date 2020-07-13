0c3f1bbe478c8849126c8b72dbf7681cb590b27a
DebDoDab <debbius3@gmail.com>
2020-07-13 14:23:53 +0300

    Release version

--- app/main/schemas.py
+++ app/main/schemas.py
@@ -2,17 +2,21 @@ from pydantic import BaseModel
 
 
 class Url(BaseModel):
+    """FastAPI schema for getting URL"""
     url: str
 
 
 class LinkBase(BaseModel):
+    """Base schema for returning Link in JSON"""
     link: str
 
 
 class LinkCreate(LinkBase):
+    """Schema for returning Link in JSON after it creation"""
     url: str
 
 
 class Link(LinkBase):
+    """Schema for returning link in JSON"""
     class Config:
         orm_mode = True
